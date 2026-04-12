db73053daf89c41c9879ede92c1427ce3ed18373
miguel-124 <mikicuellar20@gmail.com>
2026-04-12 16:27:28 -0400

    New methods to paint: DrawGhostPiece and DrawCurrentPiece.

--- Tetris/Views/BoardRenderer.cs
+++ Tetris/Views/BoardRenderer.cs
@@ -14,7 +14,7 @@ namespace Tetris.Views
 
         private Texture2D _blankTexture;
 
-        private readonly int _blockSize = 20;
+        private readonly int _blockSize = Constants.BlockSize;
 
         private readonly Vector2 _boardOffset = new(234, 19);
         private readonly Vector2 _boardSize = new(222, 442);
@@ -22,22 +22,20 @@ namespace Tetris.Views
 
         public void LoadContent(ContentManager content)
         {
-            // Creamos una textura de 1 píxel por 1 píxel
             _blankTexture = new Texture2D(_gdm.GraphicsDevice, 1, 1);
-
-            // La pintamos de blanco puro. (Al ser blanca, SpriteBatch la puede teñir de cualquier color luego)
             _blankTexture.SetData([Color.White]);
         }
 
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
-
             var board = new Rectangle((int)_boardOffset.X, (int)_boardOffset.Y, (int)_boardSize.X, (int)_boardSize.Y);
             var colorEmpty = _gameState.GameBoard.Empty.ToXnaColor();
-            spriteBatch.Draw(_blankTexture, board, colorEmpty);
+            DrawBox(spriteBatch, board, colorEmpty);
 
             DrawAllPieces(spriteBatch);
             DrawGrid(spriteBatch);
+            DrawGhostPiece(spriteBatch);
+            DrawCurrentPiece(spriteBatch);
         }
 
         private void DrawGrid(SpriteBatch spriteBatch)
@@ -49,15 +47,29 @@ namespace Tetris.Views
             { 
                 int lineX = (int)_boardOffset.X + (x * (_blockSize + _gridSize));
                 var lineRect = new Rectangle(lineX, (int)_boardOffset.Y, _gridSize, (int)_boardSize.Y);
-                spriteBatch.Draw(_blankTexture, lineRect, colorGrids);
+                DrawBox(spriteBatch, lineRect, colorGrids);
             }
             // Dibujar líneas horizontales
             for (int y = 0; y <= board.Row; y++)
             {
                 int lineY = (int)_boardOffset.Y + (y * (_blockSize + _gridSize));
                 var lineRect = new Rectangle((int)_boardOffset.X, lineY, (int)_boardSize.X, _gridSize);
-                spriteBatch.Draw(_blankTexture, lineRect, colorGrids);
+                DrawBox(spriteBatch, lineRect, colorGrids);
             }
+        }
+
+        private void DrawCurrentPiece(SpriteBatch spriteBatch)
+        {
+            var currentPiece = _gameState.CurrentPiece;
+            foreach (var coord in currentPiece.Cords)
+                DrawBlockPiece(spriteBatch, coord.X, coord.Y, currentPiece.Color.ToXnaColor());
+        }
+
+        private void DrawGhostPiece(SpriteBatch spriteBatch)
+        {
+            var ghostPiece = _gameState.GameBoard.GhostCoords;
+            foreach (var coord in ghostPiece)
+                DrawBlockPiece(spriteBatch, coord.X, coord.Y, Color.White, true);
         }
 
         private void DrawAllPieces(SpriteBatch spriteBatch)
@@ -69,13 +81,24 @@ namespace Tetris.Views
                     BlockColor colorPiece = board.GetValue(x, y);
                     Color renderColor = colorPiece.ToXnaColor();
 
-                    int drawX = (int)_boardOffset.X + (x * (_blockSize + _gridSize));
-                    int drawY = (int)_boardOffset.Y + (y * (_blockSize + _gridSize));
-
-                    var destinationRect = new Rectangle(drawX, drawY, _blockSize, _blockSize);
-                    spriteBatch.Draw(_blankTexture, destinationRect, renderColor);
+                    if (colorPiece != board.Empty)
+                        DrawBlockPiece(spriteBatch, x, y, renderColor);
                 }
         }
+
+        private void DrawBlockPiece(SpriteBatch spriteBatch, int x, int y, Color color, bool isGhost = false)
+        {
+            if (isGhost) color *= 0.3f;
+
+            int drawX = (int)_boardOffset.X + (x * (_blockSize + _gridSize));
+            int drawY = (int)_boardOffset.Y + (y * (_blockSize + _gridSize));
+
+            var destinationRect = new Rectangle(drawX, drawY, _blockSize, _blockSize);
+            DrawBox(spriteBatch, destinationRect, color);
+        }
+
+        private void DrawBox(SpriteBatch spriteBatch, Rectangle position, Color color)
+            => spriteBatch.Draw(_blankTexture, position, color);
 
         public void UnLoad()
         {
